54839bd03cabde8bef857661b5b22816093badf8
Marinos.Sake <protutor13@hotmail.com>
2025-09-26 18:28:49 +0300

    chore:initial commit

---
+++ src/main/java/com/quizai/quizplatform/QuizForTestsApplication.java
@@ -0,0 +1,13 @@
+package com.quizai.quizplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizForTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizForTestsApplication.class, args);
+	}
+
+}
