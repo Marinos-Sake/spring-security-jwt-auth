4b3e9c6b493f4599a498ce84fa3b59317a723fe1
Marinos.Sake <protutor13@hotmail.com>
2025-09-26 18:28:40 +0300

    chore:initial commit

---
+++ src/test/java/com/quizai/quizplatform/QuizForTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizai.quizplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizForTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
