49dbac3dd3e0d3b2af1e4628dac74b98e2ea753e
Marinos.Sake <protutor13@hotmail.com>
2025-09-28 22:06:58 +0300

    feat(dto): add LoginResponseDTO with JWT token field

---
+++ src/main/java/com/quizai/quizplatform/dto/LoginResponseDTO.java
@@ -0,0 +1,11 @@
+package com.quizai.quizplatform.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponseDTO {
+
+    private String token;
+}
