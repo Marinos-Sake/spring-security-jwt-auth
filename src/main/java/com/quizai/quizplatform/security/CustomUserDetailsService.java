49f700e326e5c3801aa819ec52b02cec49a14ef8
Marinos.Sake <protutor13@hotmail.com>
2025-09-28 22:06:29 +0300

    refactor(auth): use @RequiredArgsConstructor in CustomUserDetailsService

--- src/main/java/com/quizai/quizplatform/security/CustomUserDetailsService.java
+++ src/main/java/com/quizai/quizplatform/security/CustomUserDetailsService.java
@@ -2,19 +2,17 @@ package com.quizai.quizplatform.security;
 
 import com.quizai.quizplatform.entity.User;
 import com.quizai.quizplatform.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
-
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
