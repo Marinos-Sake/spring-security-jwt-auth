a30af371338355d55985e11fc5f99f4b52cf9cbd
Marinos.Sake <protutor13@hotmail.com>
2025-09-28 22:06:46 +0300

    feat(dto): add LoginRequestDTO with validation for username and password

---
+++ src/main/java/com/quizai/quizplatform/dto/LoginRequestDTO.java
@@ -0,0 +1,21 @@
+package com.quizai.quizplatform.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequestDTO {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 5, max = 16, message = "Username must be 5-16 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 5, max = 20, message = "Password must be 5-20 characters")
+    private String password;
+}
