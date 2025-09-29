e4e01d8a8030cac83d018da5b32658eabc0303e1
Marinos.Sake <protutor13@hotmail.com>
2025-09-29 18:44:56 +0300

    feat(exception): add AlreadyExists exception extending AppGenericException

---
+++ src/main/java/com/quizai/quizplatform/core/exception/AppObjectAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.quizai.quizplatform.core.exception;
+
+
+public class AppObjectAlreadyExistsException extends AppGenericException {
+
+    private static final String DEFAULT_CODE = "ALREADY_EXISTS";
+
+    public AppObjectAlreadyExistsException(String code, String message) {
+        super(code + DEFAULT_CODE, message);
+    }
+}
