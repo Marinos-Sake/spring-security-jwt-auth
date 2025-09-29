dff527399375ae28bb65bd15e32d50600eafb9ea
Marinos.Sake <protutor13@hotmail.com>
2025-09-29 18:50:53 +0300

    feat(user): add existsByUsername method to UserRepository

--- src/main/java/com/quizai/quizplatform/repository/UserRepository.java
+++ src/main/java/com/quizai/quizplatform/repository/UserRepository.java
@@ -10,4 +10,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByPublicId(String publicId);
+
+    boolean existsByUsername(String username);
 }
