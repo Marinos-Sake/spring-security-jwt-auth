39ea84ab76979d581826c02a9625ea4dc60b5f86
Marinos.Sake <protutor13@hotmail.com>
2025-10-01 16:37:03 +0300

    feat(cache): add userProfileCache bean with Caffeine

--- src/main/java/com/quizai/quizplatform/config/CacheConfig.java
+++ src/main/java/com/quizai/quizplatform/config/CacheConfig.java
@@ -2,6 +2,7 @@ package com.quizai.quizplatform.config;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.quizai.quizplatform.dto.UserReadOnlyDTO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,8 +15,16 @@ public class CacheConfig {
     @Bean
     public Cache<String, UserDetails> userDetailsCache() {
         return Caffeine.newBuilder()
-                .expireAfterWrite(Duration.ofMinutes(15)) //TTL 15 minutes
-                .maximumSize(1000) // users
+                .expireAfterWrite(Duration.ofMinutes(15)) // TTL 15 min
+                .maximumSize(1000)  //users
+                .build();
+    }
+
+    @Bean
+    public Cache<String, UserReadOnlyDTO> userProfileCache() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(Duration.ofMinutes(15))
+                .maximumSize(1000)
                 .build();
     }
 }
