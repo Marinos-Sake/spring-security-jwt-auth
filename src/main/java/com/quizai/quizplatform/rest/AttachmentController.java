4ec547c0f2d9ac471e5120930c09621115859cf7
Marinos.Sake <protutor13@hotmail.com>
2025-10-04 19:49:47 +0300

    fix(api/attachment): set consumes multipart/form-data and produces application/json; switch to @RequestPart("file")

--- src/main/java/com/quizai/quizplatform/rest/AttachmentController.java
+++ src/main/java/com/quizai/quizplatform/rest/AttachmentController.java
@@ -6,6 +6,7 @@ import com.quizai.quizplatform.dto.AttachmentReadOnlyDTO;
 import com.quizai.quizplatform.service.AttachmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -20,10 +21,14 @@ public class AttachmentController {
 
     private final AttachmentService attachmentService;
 
-    @PostMapping("/upload")
+    @PostMapping(
+            path = "/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<AttachmentReadOnlyDTO> upload(
             @AuthenticationPrincipal(expression = "publicId") String publicId,
-            @RequestParam("file") MultipartFile file
+            @RequestPart("file") MultipartFile file
     ) {
         AttachmentReadOnlyDTO dto = attachmentService.upload(publicId, file);
         return ResponseEntity.status(HttpStatus.CREATED).body(dto);
